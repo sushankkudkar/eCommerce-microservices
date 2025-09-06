c779bcaf8c6799740d8013261459d063329dc0c3
sushankkudkar <sushank.kudkar@gmail.com>
2025-09-06 12:50:24 +0530

    chore: remove all files for scan

--- order/src/main/java/com/ecommerce/order/model/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.ecommerce.order.model;
-
-public enum OrderStatus {
-    PENDING, CONFIRMED, CANCEL, DELIVERED
-}
--- order/src/main/java/com/ecommerce/order/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.order.repository;
-
-import com.ecommerce.order.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
